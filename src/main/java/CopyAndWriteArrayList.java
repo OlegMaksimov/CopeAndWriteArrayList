190b17a27dc5a0776233755d7732bc1db6e1c26e
OlegMaksimov <maksimovoleg19@gmail.com>
2019-02-03 21:05:31 +0300

    add new methods add(), length()

--- src/main/java/CopyAndWriteArrayList.java
+++ src/main/java/CopyAndWriteArrayList.java
@@ -1,20 +1,98 @@
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *
  */
 public class CopyAndWriteArrayList {
     private final Logger log = Logger.getLogger(this.getClass());
+
+    private ReentrantLock lock = new ReentrantLock();
     private int DEFAULT_CAPACITY = 10;
     private volatile int[] innerArray = new int[DEFAULT_CAPACITY];
+    private volatile int size = DEFAULT_CAPACITY;
+    private AtomicInteger cursor = new AtomicInteger();
+
 
     /**
      * Add new element in Array
-     * @return
+     * @return Boolean
      */
-    public synchronized boolean  add (){
-        boolean result = false;
-        return result;
+    public  boolean add (Integer e){
+        int[] tempArray = innerArray.clone();
+        if (cursor.get() == 0 && size >0){
+            tempArray[cursor.get()] = e;
+            lock.lock();
+            innerArray = tempArray.clone();
+            cursor.incrementAndGet();
+            lock.unlock();
+            return true;
+        }
+        if (cursor.get() < size) {
+            tempArray[cursor.get()] = e;
+            lock.lock();
+            if (checkArrays(tempArray)) {
+                innerArray = tempArray.clone();
+                cursor.incrementAndGet();
+            } else {
+                cursor.decrementAndGet();
+            }
+            lock.unlock();
+            return true;
+        } else {
+            lock.lock();
+            size = size * 2;
+            innerArray = Arrays.copyOf(innerArray,size);
+            tempArray = innerArray.clone();
+            tempArray[cursor.get()] = e;
+
+            if (checkArrays(tempArray)) {
+                innerArray = tempArray.clone();
+                cursor.incrementAndGet();
+            } else {
+                cursor.decrementAndGet();
+            }
+            lock.unlock();
+            return true;
+        }
     }
 
+    /**
+     * Check Array before write new value
+     * @param newArray  ...
+     * @return boolean
+     */
+    private  boolean checkArrays(int[] newArray){
+        if (newArray.length == 0){
+            return true;
+        }
+        int[] tempArray =  new int[newArray.length-1];
+        System.arraycopy(newArray,0,tempArray,0,newArray.length-1);
+        return innerArray.equals(tempArray);
+    }
+
+
+    /**
+     * return length of Array
+     * @return int
+     */
+    public int length() {
+        return cursor.get();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CopyAndWriteArrayList list = (CopyAndWriteArrayList) o;
+        return Arrays.equals(innerArray, list.innerArray);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(innerArray);
+    }
 }
--- src/test/java/CopyAndWriteArrayListTest.java
+++ src/test/java/CopyAndWriteArrayListTest.java
@@ -1,10 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class CopyAndWriteArrayListTest {
 
     @org.junit.jupiter.api.Test
     void add() {
         CopyAndWriteArrayList list = new CopyAndWriteArrayList();
-        list.add();
+        Assertions.assertTrue(list.add(2));
     }
+
+    @Test
+    void length() {
+        CopyAndWriteArrayList list = new CopyAndWriteArrayList();
+        int size = 15;
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        Assertions.assertEquals(size, list.length());
+    }
+
+    @Test
+    void equals() {
+        CopyAndWriteArrayList list = new CopyAndWriteArrayList();
+        CopyAndWriteArrayList list2 = new CopyAndWriteArrayList();
+        int size = 15;
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+            list2.add(i);
+        }
+        Assertions.assertEquals(list, list2);
+    }
+
 }
