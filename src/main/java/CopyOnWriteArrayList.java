948246bd0926d305207ceda469eabcb9ce7ceefb
oe.maksimov <maksimovoleg19@gmail.com>
2019-02-04 17:08:10 +0300

    Изменил методы добавления в массив

---
+++ src/main/java/CopyOnWriteArrayList.java
@@ -0,0 +1,111 @@
+import org.apache.log4j.Logger;
+
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ */
+public class CopyOnWriteArrayList extends AbstractList<Integer> {
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private int DEFAULT_CAPACITY = 10;
+    private volatile int[] innerArray = new int[DEFAULT_CAPACITY];
+    private volatile AtomicInteger cursor = new AtomicInteger();
+
+    public CopyOnWriteArrayList() {
+    }
+
+    @Override
+    public int size() {
+        return innerArray.length;
+    }
+
+    /**
+     *
+     * @param size count element оf array
+     */
+    public CopyOnWriteArrayList(int size) {
+        innerArray = new int[size];
+    }
+
+    /**
+     * Add new element in Array
+     * @return Boolean
+     */
+    public boolean add (Integer e){
+        Lock lock = this.lock;
+        lock.lock();
+        try {
+            int length = innerArray.length;
+            int[] copy = new int[length + 1];
+            System.arraycopy(innerArray, 0, copy, 0, length - 1);
+            copy[length] = e;
+
+            innerArray = copy;
+            return true;
+        } finally {
+          lock.unlock();
+        }
+    }
+
+    @Override
+    public void add(int index, Integer element) {
+        if (index > innerArray.length || index < 0) {
+            throw new IndexOutOfBoundsException("This index is not valid.");
+        }
+
+    }
+
+    @Override
+    public Integer get(int index) {
+        return innerArray[index];
+    }
+
+
+    /**
+     * Check Array before write new value
+     * @param newArray  ...
+     * @return boolean
+     */
+    private  boolean checkArrays(int[] newArray){
+        if (newArray.length == 0){
+            return true;
+        }
+        CopyOnWriteArrayList tempArray =  new CopyOnWriteArrayList(cursor.get());
+        CopyOnWriteArrayList tempInnerArray =  new CopyOnWriteArrayList(cursor.get());
+        for (int i = 0; i < cursor.get(); i++) {
+            tempArray.add(newArray[i]);
+            tempInnerArray.add(innerArray[i]);
+        }
+        return tempArray.equals(tempInnerArray);
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CopyOnWriteArrayList list = (CopyOnWriteArrayList) o;
+        return Arrays.equals(innerArray, list.innerArray);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(innerArray);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < cursor.get(); i++) {
+            buffer.append(innerArray[i]+",");
+        }
+        return buffer.toString();
+    }
+}
---
+++ src/test/java/CopyOnWriteArrayListTest.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class CopyOnWriteArrayListTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        Assertions.assertTrue(list.add(2));
+    }
+
+    @Test
+    void get(){
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        Assertions.assertTrue(list.add(2));
+        Assertions.assertEquals(2,list.get(0));
+    }
+
+    @Test
+    void length() {
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        int size = 15;
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        Assertions.assertEquals(size, list.size());
+    }
+
+    @Test
+    void equals() {
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        CopyOnWriteArrayList list2 = new CopyOnWriteArrayList();
+        int size = 15;
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+            list2.add(i);
+        }
+        Assertions.assertEquals(list, list2);
+    }
+
+    @Test
+    void racingThread() throws InterruptedException {
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            final Random random = new Random();
+            service.submit(()->{
+                list.add(random.nextInt());
+            });
+        }
+        service.awaitTermination(10, TimeUnit.SECONDS);
+        Assertions.assertEquals(110, list.size());
+        System.out.println(list.toString());
+    }
+
+    @Test
+    void getIndexOutofBounds(){
+        CopyOnWriteArrayList list = new CopyOnWriteArrayList();
+        Executable executable = ()->list.add(100,1);
+        Assertions.assertThrows(IndexOutOfBoundsException.class, executable);
+    }
+
+
+    @Test
+    void addNewEllementInCurrentIndex(){}
+
+}
